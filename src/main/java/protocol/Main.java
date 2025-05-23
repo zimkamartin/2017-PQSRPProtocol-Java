0da4d6407f8cb324f1671309324d63e3e5ba004a
zimkamartin <martin.zimka@gmail.com>
2025-05-23 17:31:44 +0200

    ADD: Maven magic around Bouncy Castle.

--- src/main/java/protocol/Main.java
+++ src/main/java/protocol/Main.java
@@ -1,17 +1,19 @@
 package protocol;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import org.bouncycastle.crypto.digests.SHA3Digest;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+
+    public static void main(String[] args) {
+        createSeeds();
+    }
+
+    private static void createSeeds() {
+        // seed1 = SHA3-256(salt||SHA3-256(I||pwd))
+        final SHA3Digest digest = new SHA3Digest(256);
+        digest.update("".getBytes(), 0, 0);
+        byte[] output = new byte[32];
+        digest.doFinal(output, 0);
+        // seed2 = SHA3-256(seed1)
     }
 }
