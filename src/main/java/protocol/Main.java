46b1a191787a8680c20ba2a9a3ed7bd21167f717
zimkamartin <martin.zimka@gmail.com>
2025-05-23 20:24:54 +0200

    ADD: Generating seed1 and seed2.

--- src/main/java/protocol/Main.java
+++ src/main/java/protocol/Main.java
@@ -1,19 +1,42 @@
 package protocol;
 
 import org.bouncycastle.crypto.digests.SHA3Digest;
+import protocol.utils.Seeds;
 
 public class Main {
 
+    // THIS IS NOT HOW TO DO IT !!! THIS IS JUST FOR PROOF-OF-CONCEPT !!! THIS IS NOT HOW TO DO IT
+    private static final String I = "identity123";
+    private static final String PWD = "password123";
+    private static final byte[] SALT = "salt123".getBytes();
+    // THIS IS NOT HOW TO DO IT !!! THIS IS JUST FOR PROOF-OF-CONCEPT !!! THIS IS NOT HOW TO DO IT
+
+    private static final SHA3Digest sha3Digest256 = new SHA3Digest(256);
+
     public static void main(String[] args) {
-        createSeeds();
+        Seeds seeds = createSeeds();
     }
 
-    private static void createSeeds() {
+    private static Seeds createSeeds() {
         // seed1 = SHA3-256(salt||SHA3-256(I||pwd))
-        final SHA3Digest digest = new SHA3Digest(256);
-        digest.update("".getBytes(), 0, 0);
-        byte[] output = new byte[32];
-        digest.doFinal(output, 0);
+        String innerInput = I.concat(PWD);
+        sha3Digest256.reset();
+        sha3Digest256.update(innerInput.getBytes(), 0, innerInput.length());
+        byte[] innerHash = new byte[32];
+        sha3Digest256.doFinal(innerHash, 0);
+        byte[] outerInput = new byte[SALT.length + innerHash.length];
+        System.arraycopy(SALT, 0, outerInput, 0, SALT.length);
+        System.arraycopy(innerHash, 0, outerInput, SALT.length, innerHash.length);
+        sha3Digest256.reset();
+        sha3Digest256.update(outerInput, 0, outerInput.length);
+        byte[] seed1 = new byte[32];
+        sha3Digest256.doFinal(seed1, 0);
         // seed2 = SHA3-256(seed1)
+        sha3Digest256.reset();
+        sha3Digest256.update(seed1, 0, seed1.length);
+        byte[] seed2 = new byte[32];
+        sha3Digest256.doFinal(seed2, 0);
+
+        return new Seeds(seed1, seed2);
     }
 }
---
+++ src/main/java/protocol/utils/Seeds.java
@@ -0,0 +1,11 @@
+package protocol.utils;
+
+public class Seeds {
+    public final byte[] seed1;
+    public final byte[] seed2;
+
+    public Seeds(byte[] seed1, byte[] seed2) {
+        this.seed1 = seed1;
+        this.seed2 = seed2;
+    }
+}
