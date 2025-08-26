871cf068d6954429e627cbae9851600d48f5c8fb
zimkamartin <martin.zimka@gmail.com>
2025-08-26 21:37:53 +0200

    ADD: Documentation.

--- src/main/java/protocol/Server.java
+++ src/main/java/protocol/Server.java
@@ -3,10 +3,38 @@ package protocol;
 import java.math.BigInteger;
 import java.util.List;
 
+/**
+ * Server from https://eprint.iacr.org/2017/1196.pdf
+ */
 public interface Server {
+
+    /**
+     * @return n, q, eta
+     */
     PublicParams getPublicParams();
+
+    /**
+     * @return public seed used to generate public polynomial a
+     */
     byte[] generatePublicSeedForA();
+
+    /**
+     * @param I - client's identity
+     * @param salt - client's salt
+     * @param vNtt- polynomial representing client's verifier in NTT form
+     */
     void enrollClient(String I, byte[] salt, List<BigInteger> vNtt);
+
+    /**
+     * @param I - client's identity
+     * @param pi - polynomial representing client's ephemeral public key
+     * @return client's salt, polynomial representing server's ephemeral public key, polynomial where coefficients are result of applying Signal function
+     */
     SaltEphPublicSignal computeSharedSecret(String I, BigInteger[] pi);
+
+    /**
+     * @param m1 - hash of concatenated client's ephemeral key with server's ephemeral key and with client's shared secret key
+     * @return hash of concatenated client's ephemeral key with m1 with server's shared secret key
+     */
     byte[] verifyEntities(byte[] m1);
 }
