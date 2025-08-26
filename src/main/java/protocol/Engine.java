2e604b956a1cf343b75f842bb011a5027a16264a
zimkamartin <martin.zimka@gmail.com>
2025-08-26 21:48:22 +0200

    ADD: Documentation.

--- src/main/java/protocol/Engine.java
+++ src/main/java/protocol/Engine.java
@@ -1,13 +1,10 @@
 package protocol;
 
-import org.bouncycastle.crypto.digests.SHA3Digest;
-import org.bouncycastle.crypto.digests.SHAKEDigest;
-import java.security.SecureRandom;
-
 /**
  * Represents engine of the protocol, so all functions that must have some internal state during the run of the protocol.
  * <p>
- * Ok, must is maybe too strong - we do not want to create repeatedly new instances of SecureRandom(), ..., so we are using this solution.
+ * Ok, must is maybe too strong - we do not want to create repeatedly new instances of SecureRandom(), ...,
+ * so we are using this solution.
  * Represents XOF function - SHAKE128, hash function - SHA3-256, and random function - SecureRandom().
  * Heavily inspired by
  * https://github.com/bcgit/bc-java/blob/main/core/src/main/java/org/bouncycastle/pqc/crypto/mlkem/Symmetric.java
@@ -15,48 +12,44 @@ import java.security.SecureRandom;
  * https://github.com/bcgit/bc-java/blob/main/core/src/main/java/org/bouncycastle/pqc/crypto/mlkem/MLKEMEngine.java
  * </p>
  */
-public class Engine {
-    private static final int xofBlockBytes = 168;
-    private final SHAKEDigest xof;
-    private final SHA3Digest sha3Digest256;
-    private final SHAKEDigest shakeDigest;
-    private final SecureRandom random;
+public interface Engine {
 
-    Engine() {
-        this.xof = new SHAKEDigest(128);
-        this.sha3Digest256 = new SHA3Digest(256);
-        this.shakeDigest = new SHAKEDigest(256);
-        this.random = new SecureRandom();
-    }
+    /**
+     * @return the output block size of the underlying XOF (SHAKE128), in bytes
+     */
+    int getXofBlockBytes();
 
-    int getXofBlockBytes() {
-        return xofBlockBytes;
-    }
+    /**
+     * @param seed - used for seeding XOF (SHAKE128)
+     */
+    void xofAbsorb(byte[] seed);
 
-    void xofAbsorb(byte[] seed) {
-        xof.reset();
-        xof.update(seed, 0, seed.length);
-    }
+    /**
+     * @param out - byte array where hash will be squeezed to
+     * @param outOffset - position from which hash will be squeezed to
+     * @param outLen - how many bytes to squeeze
+     */
+    void xofSqueezeBlocks(byte[] out, int outOffset, int outLen);
 
-    void xofSqueezeBlocks(byte[] out, int outOffset, int outLen) {
-        xof.doOutput(out, outOffset, outLen);
-    }
+    /**
+     * @param out - byte array where hashed will be put
+     * @param in - input for the hash function
+     */
+    void hash(byte[] out, byte[] in);
 
-    void hash(byte[] out, byte[] in) {
-        sha3Digest256.update(in, 0, in.length);
-        sha3Digest256.doFinal(out, 0);
-    }
+    /**
+     * @param out - byte array where the output from pseudo-random function will be put
+     * @param seed - seed for the pseudo-random function
+     */
+    void prf(byte[] out, byte[] seed);
 
-    void prf(byte[] out, byte[] seed) {
-        shakeDigest.update(seed, 0, seed.length);
-        shakeDigest.doFinal(out, 0, out.length);
-    }
+    /**
+     * @param buf - byte array which will be filed by random bytes
+     */
+    public void getRandomBytes(byte[] buf);
 
-    public void getRandomBytes(byte[] buf) {
-        this.random.nextBytes(buf);
-    }
-
-    public int getRandomBit() {
-        return this.random.nextInt(2);
-    }
+    /**
+     * @return random bit
+     */
+    public int getRandomBit();
 }
