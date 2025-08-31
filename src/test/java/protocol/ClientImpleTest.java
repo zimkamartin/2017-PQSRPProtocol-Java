e742a51249a2d30c995681973a2ccf792839f0f7
zimkamartin <martin.zimka@gmail.com>
2025-08-31 15:33:55 +0200

    ADD: Tests for just client.

---
+++ src/test/java/protocol/ClientImpleTest.java
@@ -0,0 +1,43 @@
+package protocol;
+
+import org.junit.jupiter.api.Test;
+import protocol.exceptions.NotEnrolledClientException;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClientImpleTest {
+
+    private static final int N = 4;
+    private static final BigInteger Q = BigInteger.valueOf(17);
+    private static final int ETA = 3;
+    private static final byte[] PUBLICSEEDFORA = "publicSeedForA123".getBytes();
+    private static final byte[] I = "identity123".getBytes();
+    private static final byte[] PWD = "password123".getBytes();
+    private static final byte[] SALT = "salt123".getBytes();
+    private static final List<BigInteger> VNTT = Arrays.asList(BigInteger.valueOf(0), BigInteger.valueOf(1), BigInteger.valueOf(2), BigInteger.valueOf(3));
+    private static final List<BigInteger> PINTT = Arrays.asList(BigInteger.valueOf(4), BigInteger.valueOf(5), BigInteger.valueOf(6), BigInteger.valueOf(7));
+
+    @Test
+    void computeSharedSecretClientExists() {
+        ClientsSecrets cs = new ClientsSecrets(I, PWD);
+        Server server = new ServerImple(N, Q, ETA);
+        ClientImple client = new ClientImple(server);
+        client.enrollClient(cs);
+        assertDoesNotThrow(() -> client.computeSharedSecret(cs));
+    }
+
+    @Test
+    void computeSharedSecretClientDoesNotExist() {
+        byte[] notEnrolledI = "notEnrolledI".getBytes();
+        ClientsSecrets cs = new ClientsSecrets(I, PWD);
+        Server server = new ServerImple(N, Q, ETA);
+        ClientImple client = new ClientImple(server);
+        client.enrollClient(cs);
+        assertThrows(NotEnrolledClientException.class,
+                () -> client.computeSharedSecret(new ClientsSecrets(notEnrolledI, PWD)));
+    }
+}
