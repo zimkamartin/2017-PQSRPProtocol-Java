7bc3a609982a2e34978cc69d409ba3af8dddf43e
zimkamartin <martin.zimka@gmail.com>
2025-10-01 23:44:54 +0200

    ADD: Beginning of protocol tests.

--- src/test/java/protocol/ProtocolTest.java
+++ src/test/java/protocol/ProtocolTest.java
@@ -10,14 +10,16 @@ import protocol.server.ServerImple;
 import protocol.server.TestServerWrapper;
 
 import java.math.BigInteger;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class ProtocolTest {
 
-    private static final int NUMBEROFROUNDS = 127;  // make sure that it is not larger than 127
-    // (because of the way of changing client's identity and password so they are unique)
+    private static final int NUMBEROFROUNDS = 111;
     private static final int N = 1024;
     private static final BigInteger Q = BigInteger.valueOf(1073479681);
     private static final int ETA = 3;
@@ -27,28 +29,32 @@ public class ProtocolTest {
     private static final ByteArrayWrapper PWD = new ByteArrayWrapper("password123".getBytes());
     // THIS IS NOT HOW TO DO IT !!! THIS IS JUST FOR PROOF-OF-CONCEPT !!! THIS IS NOT HOW TO DO IT !!!
 
-    private ByteArrayWrapper generateNewI(int round) {
+    Random random = new Random();
+
+    private ByteArrayWrapper generateRandomI() {
         byte[] identity = I.getData();
-        identity[identity.length - 1] = (byte) round;
+        identity[identity.length - 1] = (byte) random.nextInt(256);
         return new ByteArrayWrapper(identity);
     }
 
-    private ByteArrayWrapper generateNewPWD(int round) {
+    private ByteArrayWrapper generateRandomPWD() {
         byte[] password = PWD.getData();
-        password[password.length - 1] = (byte) round;
+        password[password.length - 1] = (byte) random.nextInt(256);
         return new ByteArrayWrapper(password);
     }
 
+    /**
+     * Tests {@code NUMBEROFROUNDS}-times that the same client with the (same) server will at the end have the same key.
+     */
     @Test
-    public void bothPartiesSameKey() {
+    public void samePartiesBothSameKey() {
 
         for (int i = 0; i < NUMBEROFROUNDS; i++) {
 
             Server delegate = new ServerImple(new RandomCustomImple(N, Q, ETA), N, Q, ETA);
             TestServerWrapper serverWrapper = new TestServerWrapper(delegate);
 
-            ClientsKnowledge ck = new ClientsKnowledge(generateNewI(i), PWD);
-            // Identity have to be different for every run since database is static.
+            ClientsKnowledge ck = new ClientsKnowledge(I, PWD);
             ClientImple client = new ClientImple(new RandomCustomImple(N, Q, ETA), serverWrapper);
 
             client.enroll(ck);
@@ -58,4 +64,91 @@ public class ProtocolTest {
             assertEquals(loginResponse.getSharedSecret(), serverWrapper.getCapturedSkj());
         }
     }
+
+    /**
+     * Tests {@code NUMBEROFROUNDS}-times that different client with the (same) server will at the end have the same key.
+     */
+    @Test
+    public void differentPartiesBothSameKey() {
+
+        for (int i = 0; i < NUMBEROFROUNDS; i++) {
+
+            Server delegate = new ServerImple(new RandomCustomImple(N, Q, ETA), N, Q, ETA);
+            TestServerWrapper serverWrapper = new TestServerWrapper(delegate);
+
+            ClientsKnowledge ck = new ClientsKnowledge(generateRandomI(), generateRandomPWD());
+            ClientImple client = new ClientImple(new RandomCustomImple(N, Q, ETA), serverWrapper);
+
+            client.enroll(ck);
+            LoginResponse loginResponse = client.login(ck);
+
+            assertTrue(loginResponse.getLoginOK());
+            assertEquals(loginResponse.getSharedSecret(), serverWrapper.getCapturedSkj());
+        }
+    }
+
+    /**
+     * Tests that {@code NUMBEROFROUNDS} different clients will at the end have different key.
+     */
+    @Test
+    public void differentClientsDifferentKeys() {
+
+        Set<ByteArrayWrapper> keys = new HashSet<>();
+
+        for (int i = 0; i < NUMBEROFROUNDS; i++) {
+
+            Server server = new ServerImple(new RandomCustomImple(N, Q, ETA), N, Q, ETA);
+
+            ClientsKnowledge ck = new ClientsKnowledge(generateRandomI(), generateRandomPWD());
+            ClientImple client = new ClientImple(new RandomCustomImple(N, Q, ETA), server);
+
+            client.enroll(ck);
+            LoginResponse loginResponse = client.login(ck);
+
+            assertTrue(loginResponse.getLoginOK());
+            keys.add(loginResponse.getSharedSecret());
+        }
+
+        assertEquals(NUMBEROFROUNDS, keys.size());
+    }
+
+    /**
+     * Tests {@code NUMBEROFROUNDS}-times that the same client will end up with different key everytime he logs to the (same) server.
+     */
+    @Test
+    public void samePartiesDifferentLoginsDifferentKeys() {
+
+        Set<ByteArrayWrapper> keys = new HashSet<>();
+
+        Server server = new ServerImple(new RandomCustomImple(N, Q, ETA), N, Q, ETA);
+
+        ClientsKnowledge ck = new ClientsKnowledge(I, PWD);
+        ClientImple client = new ClientImple(new RandomCustomImple(N, Q, ETA), server);
+
+        client.enroll(ck);
+
+        for (int i = 0; i < NUMBEROFROUNDS; i++) {
+
+            LoginResponse loginResponse = client.login(ck);
+
+            assertTrue(loginResponse.getLoginOK());
+            keys.add(loginResponse.getSharedSecret());
+        }
+
+        assertEquals(NUMBEROFROUNDS, keys.size());
+    }
+
+    /**
+     * Tests determinism of the protocol.
+     * <p>Tests {@code NUMBEROFROUNDS}-times that the same client-server with not changing ephemeral keys
+     * will end up with the same key everytime.</p>
+     */
+    @Test
+    public void samePartiesSameEphKeysSameKeys() {
+
+        // TODO
+    }
+
+    // TODO: Enrolled with one verifier, login with other => fail
+    // TODO: UNIT tests
 }
