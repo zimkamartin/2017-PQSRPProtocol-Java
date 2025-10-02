48a4a3b497c76d47c769cf178255f2abce82e493
zimkamartin <martin.zimka@gmail.com>
2025-10-02 23:44:25 +0200

    ADD: Finished protocol tests.

--- src/test/java/protocol/ProtocolTest.java
+++ src/test/java/protocol/ProtocolTest.java
@@ -4,22 +4,24 @@ import org.junit.Test;
 import protocol.client.ClientImple;
 import protocol.client.ClientsKnowledge;
 import protocol.client.LoginResponse;
+import protocol.random.PreSeededRandom;
 import protocol.random.RandomCustomImple;
 import protocol.server.Server;
+import protocol.server.ServerChangedEnrollingV;
 import protocol.server.ServerImple;
 import protocol.server.TestServerWrapper;
 
 import java.math.BigInteger;
-import java.util.HashSet;
-import java.util.Random;
-import java.util.Set;
+import java.util.*;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class ProtocolTest {
 
     private static final int NUMBEROFROUNDS = 111;
+    private static final int RANDOMISIZE = 11;
+    private static final int RANDOMPWDSIZE = 11;
+
     private static final int N = 1024;
     private static final BigInteger Q = BigInteger.valueOf(1073479681);
     private static final int ETA = 3;
@@ -29,17 +31,18 @@ public class ProtocolTest {
     private static final ByteArrayWrapper PWD = new ByteArrayWrapper("password123".getBytes());
     // THIS IS NOT HOW TO DO IT !!! THIS IS JUST FOR PROOF-OF-CONCEPT !!! THIS IS NOT HOW TO DO IT !!!
 
+    // Generate random bytes to generate random identity or random password
     Random random = new Random();
 
     private ByteArrayWrapper generateRandomI() {
-        byte[] identity = I.getData();
-        identity[identity.length - 1] = (byte) random.nextInt(256);
+        byte[] identity = new byte[RANDOMISIZE];
+        random.nextBytes(identity);
         return new ByteArrayWrapper(identity);
     }
 
     private ByteArrayWrapper generateRandomPWD() {
-        byte[] password = PWD.getData();
-        password[password.length - 1] = (byte) random.nextInt(256);
+        byte[] password = new byte[RANDOMPWDSIZE];
+        random.nextBytes(password);
         return new ByteArrayWrapper(password);
     }
 
@@ -139,16 +142,99 @@ public class ProtocolTest {
     }
 
     /**
-     * Tests determinism of the protocol.
-     * <p>Tests {@code NUMBEROFROUNDS}-times that the same client-server with not changing ephemeral keys
-     * will end up with the same key everytime.</p>
+     * Tests determinism of the protocol and correctness of the computed key.
+     * <p>Tests that {@code NUMBEROFROUNDS} client-server interactions without the randomness will end up
+     * with the same correct key everytime.</p>
      */
     @Test
-    public void samePartiesSameEphKeysSameKeys() {
+    public void determinismOneCorrectKey() {
 
-        // TODO
+        Set<ByteArrayWrapper> keys = new HashSet<>();
+        ByteArrayWrapper correctKey = new ByteArrayWrapper(new byte[] {31, 41, -100, -114, 89, 36, -83, -10, 50, 119,
+                -102, -11, -78, -21, -75, -110, -95, -64, 16, 106, 16, -70, 89, -111, 118, -102, 89, -42, 104, 95, -88, 2});
+
+        for (int i = 0; i < NUMBEROFROUNDS; i++) {
+
+            Server server = new ServerImple(new PreSeededRandom(N, Q, ETA, 123), N, Q, ETA);
+
+            ClientsKnowledge ck = new ClientsKnowledge(I, PWD);
+            ClientImple client = new ClientImple(new PreSeededRandom(N, Q, ETA, 456), server);
+
+            client.enroll(ck);
+            LoginResponse loginResponse = client.login(ck);
+
+            assertTrue(loginResponse.getLoginOK());
+            keys.add(loginResponse.getSharedSecret());
+        }
+
+        assertEquals(1, keys.size());
+        assertTrue(keys.contains(correctKey));
     }
 
-    // TODO: Enrolled with one verifier, login with other => fail
-    // TODO: UNIT tests
+    /**
+     * Tests {@code NUMBEROFROUNDS}-times that login with incorrect verifier fails.
+     */
+    @Test
+    public void incorrectVFails() {
+
+        for (int i = 0; i < NUMBEROFROUNDS; i++) {
+
+            Server delegate = new ServerImple(new RandomCustomImple(N, Q, ETA), N, Q, ETA);
+            ServerChangedEnrollingV serverWrapper = new ServerChangedEnrollingV(delegate);
+
+            ClientsKnowledge ck = new ClientsKnowledge(I, PWD);
+            ClientImple client = new ClientImple(new RandomCustomImple(N, Q, ETA), serverWrapper);
+
+            client.enroll(ck);
+            LoginResponse loginResponse = client.login(ck);
+
+            assertFalse(loginResponse.getLoginOK());
+        }
+    }
+
+    private ByteArrayWrapper generateSeededBAW(long seed) {
+
+        Random random = new Random(seed);
+
+        byte[] byteArray = new byte[123];  // randomly set value
+        random.nextBytes(byteArray);
+
+        return new ByteArrayWrapper(byteArray);
+    }
+
+    /**
+     * Tests that when {@code NUMBEROFROUNDS} parties enroll to the same server and then try to log in,
+     * every log in will end with a success.
+     */
+    @Test
+    public void enrolledMultiplePartiesLoginsSucceeded() {
+
+        Server server = new ServerImple(new RandomCustomImple(N, Q, ETA), N, Q, ETA);
+
+        ClientImple[] clients = new ClientImple[NUMBEROFROUNDS];
+
+        for (int i = 0; i < NUMBEROFROUNDS; i++) {
+
+            ByteArrayWrapper identity = generateSeededBAW(i);
+            ByteArrayWrapper password = generateSeededBAW(2L * i);
+
+            ClientsKnowledge ck = new ClientsKnowledge(identity, password);
+            ClientImple client = new ClientImple(new RandomCustomImple(N, Q, ETA), server);
+
+            client.enroll(ck);
+            clients[i] = client;
+        }
+
+        for (int i = NUMBEROFROUNDS - 1; i >= 0; i--) {
+
+            ByteArrayWrapper identity = generateSeededBAW(i);
+            ByteArrayWrapper password = generateSeededBAW(2L * i);
+
+            ClientsKnowledge ck = new ClientsKnowledge(identity, password);
+            ClientImple client = clients[i];
+
+            LoginResponse loginResponse = client.login(ck);
+            assertTrue(loginResponse.getLoginOK());
+        }
+    }
 }
---
+++ src/test/java/protocol/random/PreSeededRandom.java
@@ -0,0 +1,149 @@
+package protocol.random;
+
+import org.bouncycastle.crypto.digests.SHAKEDigest;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+import static java.math.RoundingMode.CEILING;
+import static java.math.RoundingMode.HALF_UP;
+
+public class PreSeededRandom implements RandomCustom {
+
+    private final int n;
+    private final BigInteger q;
+    private final int eta;  // CBD values will be sampled from interval [-eta; +eta]
+
+    // XOF defined in the protocol && XOF used in BC for public a (uniform distribution) from seed
+    private static final SHAKEDigest xof = new SHAKEDigest(128);
+    private static final int XOFBLOCKBYTES = xof.getByteLength();  // shall be 168
+
+    private final Random random = new Random();
+    // PRF for sampling error poly from seed
+    private final SHAKEDigest prf = new SHAKEDigest(256);
+
+    // variables used in uniform distribution:
+    private final int unifNeededNumOfBytes;
+    private final BigInteger unifMask;
+
+    public PreSeededRandom(int n, BigInteger q, int eta, long seed) {
+        this.n = n;
+        this.q = q;
+        this.eta = eta;
+        this.random.setSeed(seed);
+        // the number of needed bits for generating 1 coefficient by Uniform distribution
+        int unifNeededNumOfBits = this.q.subtract(BigInteger.ONE).bitLength();
+        this.unifNeededNumOfBytes = (unifNeededNumOfBits + 7) / 8;
+        this.unifMask = BigInteger.ONE.shiftLeft(unifNeededNumOfBits).subtract(BigInteger.ONE);
+    }
+
+    @Override
+    public byte[] getRandomBytes(int n) {
+        byte[] byteArray = new byte[n];
+        random.nextBytes(byteArray);
+        return byteArray;
+    }
+
+    @Override
+    public int getRandomInt(int bound) {
+        return random.nextInt(bound);
+    }
+
+    private int rejectionSampling(List<BigInteger> outputBuffer, int len, byte[] inpBuf, int inpBufLen) {
+        int ctr, pos;  // number of sampled coeffs and position in inpBuf
+        BigInteger val;  // candidate for coefficient
+        ctr = pos = 0;
+        while (ctr < len && pos + unifNeededNumOfBytes <= inpBufLen) {
+            byte[] bufBytes = Arrays.copyOfRange(inpBuf, pos, pos + unifNeededNumOfBytes);
+            BigInteger bufBI = new BigInteger(1, bufBytes);
+            val = bufBI.and(unifMask);
+            pos += unifNeededNumOfBytes;
+            if (val.compareTo(q) < 0) {
+                outputBuffer.add(val);
+                ctr++;
+            }
+        }
+        return ctr;
+    }
+
+    private int computeKyberGenerateMatrixNBlocks() {
+        BigDecimal numerator = BigDecimal.valueOf(n)
+                .multiply(BigDecimal.valueOf(unifNeededNumOfBytes))
+                .multiply(new BigDecimal(unifMask));
+        BigDecimal denominator = new BigDecimal(q.subtract(BigInteger.ONE));
+        BigDecimal fraction = numerator.divide(denominator, 11, HALF_UP);  // choose precision, currently set to 11 decimal places
+        BigDecimal result = fraction
+                .add(BigDecimal.valueOf(XOFBLOCKBYTES))
+                .divide(BigDecimal.valueOf(XOFBLOCKBYTES), 11, HALF_UP);
+        return result.setScale(0, CEILING).intValueExact();  // = the closest needed higher amount of xBB to generate for sampling
+    }
+
+    @Override
+    public List<BigInteger> generateUniformCoefficients(int n, byte[] seed) {
+        List<BigInteger> out = new ArrayList<>(n);
+
+        int KyberGenerateMatrixNBlocks = computeKyberGenerateMatrixNBlocks();
+
+        int k, ctr, off;
+        int buflen = KyberGenerateMatrixNBlocks * XOFBLOCKBYTES;
+        byte[] buf = new byte[buflen];
+        xof.reset();
+        xof.update(seed, 0, seed.length);
+        xof.doOutput(buf, 0, buflen);
+
+        ctr = rejectionSampling(out, n, buf, buflen);  // number of sampled coefficients
+
+        while (ctr < n) {  // we did not sample enough coeffs
+            off = buflen % unifNeededNumOfBytes;  // how many unused bytes is in buf?
+            for (k = 0; k < off; k++) {  // move unused bytes to the beginning of the buf
+                buf[k] = buf[buflen - off + k];
+            }
+            xof.doOutput(buf, off, buflen - off);  // fill the rest of buf
+            ctr += rejectionSampling(out, n - ctr, buf, buflen);
+        }
+
+        return out;
+    }
+
+    private int bitCountOfMUnusedBits(byte b, int bitIndex, int m) {
+        int inp = b & 0xFF;  // get the unsigned value of a byte
+        int shifted = inp << bitIndex;  // get rid of already used bits
+        int mask = (1 << m) - 1;  // create mask for m bits
+        return Integer.bitCount(shifted & mask);  // add to a number of bits 1 in masked result
+    }
+
+    private int readEtaBits(BitCursor bc, byte[] buf, int eta) {
+        int count = 0;  // how many bits do I already have?
+        int ones = 0;  // number of ones
+
+        while (count < eta) {
+            int m = Math.min(eta - count, 8 - bc.getBitIndex());  // how many bits will we take from the byte
+            ones += bitCountOfMUnusedBits(buf[bc.getByteIndex()], bc.getBitIndex(), m);
+            count += m;  // update number of bits that we already have
+            bc.updateBitByteIndices(m);
+        }
+
+        return ones;
+    }
+
+    @Override
+    public List<BigInteger> generateCbdCoefficients(int n, byte[] seed) {
+        List<BigInteger> out = new ArrayList<>(n);
+        byte[] buf = new byte[(int) Math.ceil((n * 2.0 * eta) / 8.0)];
+        prf.update(seed, 0, seed.length);
+        prf.doFinal(buf, 0, buf.length);
+
+        BitCursor bc = new BitCursor();
+        for (int i = 0; i < n; i++) {
+            int a = readEtaBits(bc, buf, eta);
+            int b = readEtaBits(bc, buf, eta);
+            out.add(BigInteger.valueOf(a - b));
+        }
+
+        return out;
+    }
+}
